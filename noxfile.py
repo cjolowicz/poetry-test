4ec3894249cd0378f739fec5cf263b06dc2f83c3
Claudio Jolowicz <mail@claudiojolowicz.com>
2023-04-25 21:28:05 +0200

    initial

---
+++ noxfile.py
@@ -0,0 +1,35 @@
+from typing import Iterable
+
+import nox
+
+
+def install(session: nox.Session, *, groups: Iterable[str], root: bool = True) -> None:
+    """Install the dependency groups using Poetry.
+    This function installs the given dependency groups into the session's
+    virtual environment. When ``root`` is true (the default), the function
+    also installs the root package and its default dependencies.
+    To avoid an editable install, the root package is not installed using
+    ``poetry install``. Instead, the function invokes ``pip install .``
+    to perform a PEP 517 build.
+    Args:
+        session: The Session object.
+        groups: The dependency groups to install.
+        root: Install the root package.
+    """
+    session.run_always(
+        "poetry@1.3.2",
+        "install",
+        "--no-root",
+        "--sync",
+        "--{}={}".format("only" if not root else "with", ",".join(groups)),
+        external=True,
+    )
+    if root:
+        session.install(".")
+
+
+@nox.session
+def fmt(s: nox.Session) -> None:
+    install(s, groups=["fmt"], root=False)
+    s.run("isort", ".")
+    s.run("black", ".")
